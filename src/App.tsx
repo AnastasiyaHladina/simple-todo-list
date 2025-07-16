1463a38cbc3084be9789d3e5c4169c2da68c21db
AnastasiiaHladina <anastasiia.hladina@gmail.com>
2025-07-16 16:27:37 +0300

    git init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MagnifyingGlassIcon from "./components/icons/magnifying-glass.tsx";
+
+
+function App() {
+
+  return (
+      <>
+
+      </>
+  )
+}
+
+export default App
